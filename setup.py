b0a49c76e295d998d46c14e40de8f148690b4558
duongtuan30306 <duongtuan30306@gmail.com>
2022-11-12 11:03:37 +0700

    version update

--- setup.py
+++ setup.py
@@ -5,9 +5,28 @@ try:
 except:
     long_description = ""
 
+version = '0.5.5a'
+
+if version.endswith('a'):
+    # append version identifier based on commit count
+    try:
+        import subprocess
+        p = subprocess.Popen(['git', 'rev-list', '--count', 'HEAD'],
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        if out:
+            version += out.decode('utf-8').strip()
+        p = subprocess.Popen(['git', 'rev-parse', '--short', 'HEAD'],
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        if out:
+            version += '+g' + out.decode('utf-8').strip()
+    except Exception:
+        pass
+
 setup(
     name='soundcloud-lib',
-    version='0.5.4a',
+    version=version,
     description='Python Soundcloud API',
     long_description=long_description,
     long_description_content_type='text/markdown',
